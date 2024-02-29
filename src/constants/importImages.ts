804013723c8d4aedf22dcfb4a31cbaa7d564c1ed
seongseonko <kymgok1001@naver.com>
2024-02-29 17:06:17 +0900

    feat: ImportImage구조화 ,ICON 객체화

---
+++ src/constants/importImages.ts
@@ -0,0 +1,224 @@
+import IconAccount from '/public/icons/Icon_account_check.svg';
+import IconBed from '/public/icons/Icon_bed.svg';
+import IconBell from '/public/icons/Icon_bell.svg';
+import IconCalendar from '/public/icons/Icon_calendar.svg';
+import IconCalendarCheck from '/public/icons/Icon_calendar_check.svg';
+import IconEyeOn from '/public/icons/Icon_eye_on.svg';
+import IconEyeOff from '/public/icons/Icon_eye_off.svg';
+import IconFacebook from '/public/icons/Icon_faceBook.svg';
+import IconInstagram from '/public/icons/Icon_instagram.svg';
+import IconTwitter from '/public/icons/Icon_twitter.svg';
+import IconYoutube from '/public/icons/Icon_youtube.svg';
+import IconLogoSmall from '/public/icons/Icon_logo_small.svg';
+import IconStarOn from '/public/icons/Icon_star_on.svg';
+import IconStarOff from '/public/icons/Icon_star_off.svg';
+import IconTextBoxCheck from '/public/icons/Icon_text-box_check.svg';
+import IconMenu from '/public/icons/Icon_meatball.svg';
+import IconSetting from '/public/icons/Icon_setting.svg';
+import IconCancel from '/public/icons/Icon_cancel.svg';
+import IconX from '/public/icons/Icon_X.svg';
+import IconXBold from '/public/icons/Icon_X_bold.svg';
+import IconXMedium from '/public/icons/Icon_X_medium.svg';
+import IconLeftArrow from '/public/icons/Icon_left-arrow_default.svg';
+import IconLeftArrowActive from '/public/icons/Icon_left-arrow_active.svg';
+import IconRightArrow from '/public/icons/Icon_right-arrow_default.svg';
+import IconRightArrowActive from '/public/icons/Icon_right-arrow_active.svg';
+import IconPlusTime from '/public/icons/Icon_plus_time.svg';
+import IconMinusTime from '/public/icons/Icon_minus_time.svg';
+import IconPlus from '/public/icons/Icon_plus.svg';
+import IconLeftArrowVariant1 from '/public/icons/Icon_left-arrow_variant1.svg';
+import IconRightArrowVariant1 from '/public/icons/Icon_right-arrow_variant1.svg';
+
+export const ICON = {
+  logo: {
+    default: {
+      src: IconLogoSmall,
+      alt: '로고',
+    },
+    active: {},
+  },
+  accountCheck: {
+    default: {
+      src: IconAccount,
+      alt: '계정 체크',
+    },
+    active: {},
+  },
+  bed: {
+    default: {
+      src: IconBed,
+      alt: '침대',
+    },
+    active: {},
+  },
+  bell: {
+    default: {
+      src: IconBell,
+      alt: '종',
+    },
+    active: {},
+  },
+  calendar: {
+    default: {
+      src: IconCalendar,
+      alt: '달력',
+    },
+    active: {},
+  },
+  calendarCheck: {
+    default: {
+      src: IconCalendarCheck,
+      alt: '달력 체크',
+    },
+    active: {},
+  },
+  eye: {
+    on: {
+      src: IconEyeOn,
+      alt: '뜬 눈',
+    },
+    off: {
+      src: IconEyeOff,
+      alt: '감은 눈',
+    },
+  },
+  facebook: {
+    default: {
+      src: IconFacebook,
+      alt: '페이스북 로고',
+    },
+    active: {},
+  },
+  instagram: {
+    default: {
+      src: IconInstagram,
+      alt: '인스타그램 로고',
+    },
+    active: {},
+  },
+  twitter: {
+    default: {
+      src: IconTwitter,
+      alt: '트위터 로고',
+    },
+    active: {},
+  },
+  youtube: {
+    default: {
+      src: IconYoutube,
+      alt: '유튜브 로고',
+    },
+    active: {},
+  },
+  star: {
+    default: {
+      src: IconStarOff,
+      alt: '별',
+    },
+    active: {
+      src: IconStarOn,
+      alt: '별',
+    },
+  },
+  textBoxCheck: {
+    default: {
+      src: IconTextBoxCheck,
+      alt: '메모장 체크',
+    },
+    active: {},
+  },
+  menu: {
+    default: {
+      src: IconMenu,
+      alt: '메뉴',
+    },
+    active: {},
+  },
+  setting: {
+    default: {
+      src: IconSetting,
+      alt: '세팅',
+    },
+  },
+  cancel: {
+    default: {
+      src: IconCancel,
+      alt: '취소',
+    },
+    active: {},
+  },
+  x: {
+    default: {
+      src: IconX,
+      alt: 'X',
+    },
+    active: {},
+  },
+  xBold: {
+    default: {
+      src: IconXBold,
+      alt: 'X',
+    },
+    active: {},
+  },
+  xMedium: {
+    default: {
+      src: IconXMedium,
+      alt: 'X',
+    },
+    active: {
+      alt: 'X',
+    },
+  },
+  leftArrow: {
+    default: {
+      src: IconLeftArrow,
+      alt: '좌측 화살표',
+    },
+    active: {
+      src: IconLeftArrowActive,
+      alt: '좌측 화살표',
+    },
+    variant1: {
+      src: IconLeftArrowVariant1,
+      alt: '좌측 화살표',
+    },
+  },
+  rightArrow: {
+    default: {
+      src: IconRightArrow,
+      alt: '우측 화살표',
+    },
+    active: {
+      src: IconRightArrowActive,
+      alt: '우측 화살표',
+    },
+    variant1: {
+      src: IconRightArrowVariant1,
+      alt: '우측 화살표',
+    },
+  },
+  plus: {
+    default: {
+      src: IconPlus,
+      alt: '플러스',
+    },
+    active: {},
+  },
+  plusTime: {
+    default: {
+      src: IconPlusTime,
+      alt: '플러스',
+    },
+    active: {},
+  },
+  minusTime: {
+    default: {
+      src: IconMinusTime,
+      alt: '마이너스',
+    },
+    active: {},
+  },
+};
+
+export const IMAGE = {};
