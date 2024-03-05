a45e664d7c8cf3c4e163d8910f06fcd54fa3e49b
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-03-05 20:52:45 +0900

    chore : next.config 파일에 이미지url 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
   sassOptions: {
     additionalData: '@import "@/styles/main.scss";',
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/globalnomad/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
