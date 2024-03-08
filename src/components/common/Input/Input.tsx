70eb74d4790f43bb73316d9464cf2aac72d1c8d0
seongseonko <kymgok1001@naver.com>
2024-03-08 23:56:09 +0900

    feat: Input 컴포넌트에서 textarea 분리 Textarea컴포넌트 구현

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FocusEventHandler } from 'react';
+import { DetailedHTMLProps, InputHTMLAttributes, LegacyRef, TextareaHTMLAttributes } from 'react';
 import Image from 'next/image';
 import classNames from 'classnames/bind';
 import { useToggleButton } from '@/hooks';
@@ -7,43 +7,33 @@ import style from '@/components/common/Input/inputField.module.scss';
 
 const cn = classNames.bind(style);
 
-interface InputProps {
+interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   name: string;
-  type: string;
+  type: 'text' | 'password';
   isError?: boolean;
   errorMessage?: string;
-  className?: string;
   placeholder?: string;
-  maxLength?: number;
-  value?: string | number;
-  onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
-  onBlur?: FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>;
 }
-
-export default function Input({ name, type, isError, errorMessage, className, ...props }: InputProps) {
+export default function Input({ name, type, isError, errorMessage, ...props }: InputProps) {
   const { isToggle, handleToggleClick } = useToggleButton();
   const { src, alt, inputType } = isToggle ? USER_PASSWORD_SHOW.on : USER_PASSWORD_SHOW.off;
 
   return (
     <>
-      <div className={cn(type === 'textarea' ? 'textarea-field' : 'input-field')}>
-        {type === 'textarea' ? (
-          <textarea name={name} className={cn('textarea')} {...props} />
-        ) : (
-          <>
-            <input
-              name={name}
-              type={type === 'password' ? inputType : type}
-              className={cn('input', { error: isError }, className)}
-              {...props}
-            />
-            {type === 'password' && (
-              <button type='button' onClick={handleToggleClick} className={cn('btn-password-show')}>
-                <Image src={src} alt={alt} width={24} height={24} />
-              </button>
-            )}
-          </>
-        )}
+      <div className={cn('input-field')}>
+        <>
+          <input
+            {...props}
+            name={name}
+            type={type === 'password' ? inputType : type}
+            className={cn('input', { error: isError })}
+          />
+          {type === 'password' && (
+            <button type='button' onClick={handleToggleClick} className={cn('btn-password-show')}>
+              <Image src={src} alt={alt} width={24} height={24} />
+            </button>
+          )}
+        </>
       </div>
       {isError && <p className={cn('error')}>{errorMessage}</p>}
     </>
---
+++ src/components/common/Input/Textarea.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames/bind';
+import style from '@/components/common/Input/inputField.module.scss';
+import { DetailedHTMLProps, TextareaHTMLAttributes } from 'react';
+
+const cn = classNames.bind(style);
+
+interface TextareaProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
+  name: string;
+  isError: boolean;
+  errorMessage: string;
+}
+
+export default function Textarea({ name, isError, errorMessage, ...props }: TextareaProps) {
+  return (
+    <>
+      <div className={cn('textarea-field')}>
+        <textarea {...props} name={name} className={cn('textarea', { error: isError })} />
+      </div>
+      {isError && <p className={cn('error')}>{errorMessage}</p>}
+    </>
+  );
+}
