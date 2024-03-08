0d5176b10f49971f66d27a59fb8ba2e00c44a589
박운성 <cheanju0@gmail.com>
2024-03-08 12:31:31 +0900

    feat: Filter 컴포넌트 구현

---
+++ src/components/common/filter/Filter.tsx
@@ -0,0 +1,38 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import classNames from "classnames/bind";
+import styles from "./Filter.module.scss";
+import Image from "next/image";
+import { ICON } from "@/constants/importImages";
+
+const cn = classNames.bind(styles);
+
+interface FilterProps {
+  setPriceSort: Dispatch<SetStateAction<string>>;
+}
+
+export default function Filter({ setPriceSort }: FilterProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleDropdownClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleDropdownOptionClick = (option: string) => {
+    setPriceSort(option);
+    setIsOpen(false);
+  };
+  return (
+    <div className={cn("dropdown")}>
+      <button className={cn("dropdown-button")} onClick={handleDropdownClick}>
+        <span>가격</span>
+        <Image src={ICON.filter.default.src} alt={ICON.filter.default.alt} height={22} width={22} />
+      </button>
+      {isOpen && (
+        <div className={cn("dropdown-menu")}>
+          <button onClick={() => handleDropdownOptionClick("price_asc")}>가격이 낮은 순</button>
+          <button onClick={() => handleDropdownOptionClick("price_desc")}>가격이 높은 순</button>
+        </div>
+      )}
+    </div>
+  );
+}
