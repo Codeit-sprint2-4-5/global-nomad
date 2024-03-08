67aae526dcfa7e1fb512d9a9641c21c81157b149
박운성 <cheanju0@gmail.com>
2024-03-08 17:05:46 +0900

    style: doublequote -> singlequote 수정

--- src/components/common/filter/Filter.tsx
+++ src/components/common/filter/Filter.tsx
@@ -1,8 +1,8 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import classNames from "classnames/bind";
-import styles from "./Filter.module.scss";
-import Image from "next/image";
-import { ICON } from "@/constants/importImages";
+import { Dispatch, SetStateAction, useState } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Filter.module.scss';
+import Image from 'next/image';
+import { ICON } from '@/constants/importImages';
 
 const cn = classNames.bind(styles);
 
@@ -22,15 +22,15 @@ export default function Filter({ setPriceSort }: FilterProps) {
     setIsOpen(false);
   };
   return (
-    <div className={cn("dropdown")}>
-      <button className={cn("dropdown-button", `${isOpen ? "open" : ""}`)} onClick={handleDropdownClick}>
+    <div className={cn('dropdown')}>
+      <button className={cn('dropdown-button', `${isOpen ? 'open' : ''}`)} onClick={handleDropdownClick}>
         <span>가격</span>
         <Image src={ICON.filter.default.src} alt={ICON.filter.default.alt} height={22} width={22} />
       </button>
       {isOpen && (
-        <div className={cn("dropdown-menu")}>
-          <button onClick={() => handleDropdownOptionClick("price_asc")}>가격이 낮은 순</button>
-          <button onClick={() => handleDropdownOptionClick("price_desc")}>가격이 높은 순</button>
+        <div className={cn('dropdown-menu')}>
+          <button onClick={() => handleDropdownOptionClick('price_asc')}>가격이 낮은 순</button>
+          <button onClick={() => handleDropdownOptionClick('price_desc')}>가격이 높은 순</button>
         </div>
       )}
     </div>
