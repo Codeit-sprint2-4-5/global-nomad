44afadc27736ebcdd7c442accf8583717fc04212
JaeHwan <131963909+BBoBBoM@users.noreply.github.com>
2024-03-09 15:04:45 +0900

    refactor: 케밥 외부클릭 닫힘 이벤트 제거, css 단위 수정

--- src/components/common/card/MyActivitiesCard.tsx
+++ src/components/common/card/MyActivitiesCard.tsx
@@ -14,29 +14,19 @@ interface MyActivitiesCardProps {
   onDelete: (id: number) => void;
 }
 export default function MyActivitiesCard({
-  activityInfo: activityInfo,
+  activityInfo,
   onDelete,
 }: MyActivitiesCardProps) {
   const [kebabClick, setKebabClick] = useState<boolean>(false);
   const router = useRouter();
 
-  const optionsRef = useRef<HTMLDivElement>(null);
-
-  const handleKebabClose = (e: React.FocusEvent) => {
-    if (!optionsRef.current?.contains(e.relatedTarget as Node)) {
-      setKebabClick(false);
-    }
-  };
-
   const handleKebabClick = () => {
     setKebabClick((prev) => !prev);
   };
   const handleModifyClick = (id: number) => {
-    setKebabClick(false);
-    router.push(`/my-activities/${id}`);
-  };
-  const handleActivityDetail = (id: number) => {
-    router.push(`/activities/${id}`);
+    console.log(id);
+    setKebabClick((prev) => !prev);
+    // router.push(`/my-activities/${id}`);
   };
 
   return (
@@ -47,7 +37,6 @@ export default function MyActivitiesCard({
         height={204}
         alt="배너이미지"
         className={cn("card-image")}
-        onClick={() => handleActivityDetail(activityInfo.id)}
       />
       <div className={cn("card-info")}>
         <div className={cn("card-info-first")}>
@@ -64,35 +53,36 @@ export default function MyActivitiesCard({
           </div>
           <div className={cn("card-info-title")}>{activityInfo.title}</div>
         </div>
-
         <div className={cn("info-second")}>
           <div className={cn("info-price")}>
             ￦ {activityInfo.price.toLocaleString()}
             <span>/ 인</span>
           </div>
-          <div onClick={handleKebabClick} onBlur={handleKebabClose}>
+          <div onClick={handleKebabClick}>
             <IconButton
               svg={ICON.menu.default.src}
               size="40"
               alt={ICON.menu.default.alt}
             />
-            {kebabClick && (
-              <div className={cn("context-menu")}>
-                <div
-                  className={cn("menu", "border")}
-                  onClick={() => handleModifyClick(activityInfo.id)}
-                >
-                  수정하기
-                </div>
-                <div
-                  className={cn("menu")}
-                  onClick={() => onDelete(activityInfo.id)}
-                >
-                  삭제하기
-                </div>
-              </div>
-            )}
           </div>
+          {kebabClick && (
+            <div className={cn("context-menu")}>
+              <div
+                className={cn("menu", "border")}
+                onClick={() => handleModifyClick(activityInfo.id)}
+              >
+                수정하기
+              </div>
+              <div
+                className={cn("menu")}
+                onClick={() => (
+                  onDelete(activityInfo.id), setKebabClick(false)
+                )}
+              >
+                삭제하기
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
