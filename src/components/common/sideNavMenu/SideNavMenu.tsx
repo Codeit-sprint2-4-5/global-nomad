31ac70e17a21c002cebb24193c0ba657e0e9aea5
JaeHwan <131963909+BBoBBoM@users.noreply.github.com>
2024-03-13 21:42:00 +0900

    fix: 메뉴 선택에 관한사항 프롭으로 받던부분 router로 처리 css부분 mobile일때 보여줘야하는 부분 처리

--- src/components/common/sideNavMenu/SideNavMenu.tsx
+++ src/components/common/sideNavMenu/SideNavMenu.tsx
@@ -6,15 +6,19 @@ import MENU_ITEMS from "@/constants/menuItems";
 import { ICON } from "@/constants";
 import { useRouter } from "next/router";
 const cn = classNames.bind(styles);
-interface sideNavMenuProps {
-  activeMenu: string;
-}
-export default function SideNavMenu({ activeMenu }: sideNavMenuProps) {
+export default function SideNavMenu({
+  isVisible = false,
+}: {
+  isVisible?: boolean;
+}) {
   const router = useRouter();
+  const pathname = router.pathname;
+  const selectedValue = pathname.substring("/mypage/".length);
+
   const initialValue = "/images/default_profile_image.png";
   const [profileImage, setProfileImage] = useState<string>(initialValue);
-
   const inputRef = useRef<HTMLInputElement>(null);
+
   const handleButtonClick = () => {
     if (inputRef.current) {
       inputRef.current.click();
@@ -40,7 +44,7 @@ export default function SideNavMenu({ activeMenu }: sideNavMenuProps) {
   };
 
   return (
-    <div className={cn("side-menu-entire")}>
+    <div className={cn("side-menu-entire", { isVisible: isVisible })}>
       <div className={cn("user-profile")}>
         <Image
           src={profileImage}
@@ -69,7 +73,7 @@ export default function SideNavMenu({ activeMenu }: sideNavMenuProps) {
           <li
             key={index}
             className={cn("side-menu-link", {
-              active: activeMenu === menuItem.id,
+              active: selectedValue === menuItem.id,
             })}
             onClick={() => handleMenuClick(menuItem.id)}
           >
