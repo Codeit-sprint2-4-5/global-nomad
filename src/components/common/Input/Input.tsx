910e2e0c53d28b7159da9efb5ab458bef2d7d33b
seongseonko <kymgok1001@naver.com>
2024-03-14 22:57:59 +0900

    Refactor: Input 컴포넌트 forwardRef 적용

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -4,7 +4,6 @@ import classNames from 'classnames/bind';
 import { useToggleButton } from '@/hooks';
 import { USER_PASSWORD_SHOW } from '@/constants';
 import style from '@/components/common/Input/inputField.module.scss';
-import { UseFormRegisterReturn } from 'react-hook-form';
 
 const cn = classNames.bind(style);
 
