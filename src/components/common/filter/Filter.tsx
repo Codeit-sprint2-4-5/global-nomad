1a65e79abc52015805b3518e9771bb0c3326685e
운성 <86371724+Useong0@users.noreply.github.com>
2024-03-15 16:13:49 +0900

    refactor: 오타 수정 (#93)
    * feat: Category 컴포넌트 구현
    
    * design: Category 컴포넌트 모바일 padding 값 추가
    
    * feat: Filter 컴포넌트 구현
    
    * design: 필터 클릭 시 아이콘 회전
    
    * style: doublequote -> singlequote 수정
    
    * design: 불필요한 속성 삭제
    
    * feat: FilterType 추가
    
    * feat: Menu 컴포넌트 구현
    
    * design: Filter CSS 수정
    
    * feat: Category 컴포넌트 구현
    
    * design: Category 컴포넌트 모바일 padding 값 추가
    
    * feat: Filter 컴포넌트 구현
    
    * design: 필터 클릭 시 아이콘 회전
    
    * style: doublequote -> singlequote 수정
    
    * design: 불필요한 속성 삭제
    
    * feat: FilterType 추가
    
    * feat: Menu 컴포넌트 구현
    
    * design: Filter CSS 수정
    
    * feat: DropdownMenu 컴포넌트로 구현
    
    * refactor: 조건 연산자 수정
    
    * remove: 불필요한 파일 삭제
    
    * refactor: 오타 수정
    
    * feat: Category 컴포넌트 구현
    
    * feat: FilterType 추가
    
    * design: Filter CSS 수정
    
    * feat: DropdownMenu 컴포넌트로 구현
    
    * feat: Filter 컴포넌트 구현
    
    * feat: FilterType 추가
    
    * design: Filter CSS 수정
    
    * remove: 불필요한 파일 삭제
    
    * style: 불필요한 속성 삭제

--- src/components/common/filter/Filter.tsx
+++ src/components/common/filter/Filter.tsx
@@ -9,76 +9,76 @@ import styles from './Filter.module.scss';
 const cn = classNames.bind(styles);
 
 interface FilterProps {
-  type: 'price' | 'filter';
-  setFilterState: Dispatch<SetStateAction<string>>;
+	type: 'price' | 'filter';
+	setFilterState: Dispatch<SetStateAction<string>>;
 }
 
 interface FilterTypeProps {
-  [type: string]: {
-    text: string;
-    list: {
-      text: string;
-      handleClick: () => void;
-    }[];
-  };
+	[type: string]: {
+		text: string;
+		list: {
+			text: string;
+			handleClick: () => void;
+		}[];
+	};
 }
 
 export default function Filter({ type, setFilterState }: FilterProps) {
-  const { isToggle: isOpen, handleToggleClick: isOpentoggle } = useToggleButton();
+	const { isToggle: isOpen, handleToggleClick: isOpentoggle } = useToggleButton();
 
-  const handleDropdownOptionClick = (option: string) => {
-    setFilterState(option);
-    isOpentoggle();
-  };
+	const handleDropdownOptionClick = (option: string) => {
+		setFilterState(option);
+		isOpentoggle();
+	};
 
-  const FilterType: FilterTypeProps = {
-    price: {
-      text: '가격',
-      list: [
-        {
-          text: '가격이 낮은 순',
-          handleClick: () => handleDropdownOptionClick('price_asc'),
-        },
-        {
-          text: '가격이 높은 순',
-          handleClick: () => handleDropdownOptionClick('price_desc'),
-        },
-      ],
-    },
-    filter: {
-      text: '필터',
-      list: [
-        {
-          text: '예약 신청',
-          handleClick: () => handleDropdownOptionClick('pending'),
-        },
-        {
-          text: '예약 취소',
-          handleClick: () => handleDropdownOptionClick('cancled'),
-        },
-        {
-          text: '예약 승인',
-          handleClick: () => handleDropdownOptionClick('confirmed'),
-        },
-        {
-          text: '예약 거절',
-          handleClick: () => handleDropdownOptionClick('declined'),
-        },
-        {
-          text: '체험 완료',
-          handleClick: () => handleDropdownOptionClick('completed'),
-        },
-      ],
-    },
-  };
+	const FilterType: FilterTypeProps = {
+		price: {
+			text: '가격',
+			list: [
+				{
+					text: '가격이 낮은 순',
+					handleClick: () => handleDropdownOptionClick('price_asc'),
+				},
+				{
+					text: '가격이 높은 순',
+					handleClick: () => handleDropdownOptionClick('price_desc'),
+				},
+			],
+		},
+		filter: {
+			text: '필터',
+			list: [
+				{
+					text: '예약 신청',
+					handleClick: () => handleDropdownOptionClick('pending'),
+				},
+				{
+					text: '예약 취소',
+					handleClick: () => handleDropdownOptionClick('canceled'),
+				},
+				{
+					text: '예약 승인',
+					handleClick: () => handleDropdownOptionClick('confirmed'),
+				},
+				{
+					text: '예약 거절',
+					handleClick: () => handleDropdownOptionClick('declined'),
+				},
+				{
+					text: '체험 완료',
+					handleClick: () => handleDropdownOptionClick('completed'),
+				},
+			],
+		},
+	};
 
-  return (
-    <div className={cn('dropdown')}>
-        <button className={cn('dropdown-button', { open: isOpen })} onClick={isOpentoggle}>
-          <span>{FilterType[type].text}</span>
-          <Image src={ICON.filter.default.src} alt={ICON.filter.default.alt} height={22} width={22} />
-        </button>
-      {isOpen && <DropdownMenu dropdownMenuList={FilterType[type].list} />}
-    </div>
-  );
+	return (
+		<div className={cn('dropdown')}>
+			<button className={cn('dropdown-button', { open: isOpen })} onClick={isOpentoggle}>
+				<span>{FilterType[type].text}</span>
+				<Image src={ICON.filter.default.src} alt={ICON.filter.default.alt} height={22} width={22} />
+			</button>
+			{isOpen && <DropdownMenu dropdownMenuList={FilterType[type].list} />}
+		</div>
+	);
 }
