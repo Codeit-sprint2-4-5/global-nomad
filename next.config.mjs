f8d59403ce66525e79bde46dcfdfc0d154324260
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-03-15 20:07:35 +0900

    Feat : 리뷰 모달 , 알림 모달 구현  머지 취소
    This reverts commit d2f1d3400656a8c737e0e1bfaeb1a97fc583c4ef.

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     additionalData: '@import "@/styles/main.scss";',
   },
   images: {
-    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
   },
 };
 
