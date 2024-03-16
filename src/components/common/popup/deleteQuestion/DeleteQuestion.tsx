54a403ae8bf98e13244d892c024c81f34ffd8ba4
JaeHwan <131963909+BBoBBoM@users.noreply.github.com>
2024-03-16 16:59:16 +0900

    feat: 나의 활동 삭제에 대한 popup  필요판단으로 구현

---
+++ src/components/common/popup/deleteQuestion/DeleteQuestion.tsx
@@ -0,0 +1,45 @@
+import { RefObject } from 'react';
+
+import BaseButton from '@/components/common/button/BaseButton';
+import classNames from 'classnames/bind';
+import styles from '../question/Question.module.scss';
+
+const cn = classNames.bind(styles);
+
+interface Props {
+  dialogRef: RefObject<HTMLDialogElement>;
+  activityId: number | null;
+  handleDeleteClick: (id: number) => void;
+}
+
+export default function DeleteQuestion({
+  dialogRef,
+  activityId,
+  handleDeleteClick,
+}: Props) {
+  const handleCloseClick = () => {
+    if (!dialogRef.current) return;
+    dialogRef.current.close();
+  };
+
+  return (
+    <>
+      <dialog className={cn('container')} ref={dialogRef}>
+        <p className={cn('text')}>체험을 삭제하시겠습니까??</p>
+        <div className={cn('button-group')}>
+          <BaseButton
+            onClick={handleCloseClick}
+            size={'sm'}
+            variant={'outline'}
+            text={'아니오'}
+          />
+          <BaseButton
+            onClick={() => activityId !== null && handleDeleteClick(activityId)}
+            size={'sm'}
+            text={'삭제하기'}
+          />
+        </div>
+      </dialog>
+    </>
+  );
+}
