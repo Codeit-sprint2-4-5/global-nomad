0d0bcd7498c7e754a1cfcaa3c5edae2b61bbf0e9
JaeHwan <131963909+BBoBBoM@users.noreply.github.com>
2024-03-16 20:44:26 +0900

    delete: question공통 컴포넌트 구현으로 인한 파일삭제 (#106)
    * fix: 경로 수정 및 닫기 추가
    
    * fix: 컴포넌트명 수정
    
    * feat: 나의 활동 삭제에 대한 popup  필요판단으로 구현
    
    * fix: 여러 페이지에서 쓸 수 있게 수정
    
    * delete: Question 공통으로 구현됨으로 인한 파일 삭제
    
    ---------
    
    Co-authored-by: mijin0928 <94034865+mijin0928@users.noreply.github.com>

--- src/components/common/popup/question/Question.tsx
+++ src/components/common/popup/question/Question.tsx
@@ -12,7 +12,9 @@ interface Props {
   onClick: () => void;
 }
 
+
 export default function Question({ dialogRef, text, buttonText, onClick }: Props) {
+
   const handleButtonClick = () => {
     onClick();
     handleCloseClick();
@@ -28,8 +30,10 @@ export default function Question({ dialogRef, text, buttonText, onClick }: Props
       <dialog className={cn('container')} ref={dialogRef}>
         <p className={cn('text')}>{text}</p>
         <div className={cn('button-group')}>
+
           <BaseButton onClick={handleCloseClick} size='sm' variant='outline' text='아니오' />
           <BaseButton onClick={handleButtonClick} size='sm' text={buttonText} />
+
         </div>
       </dialog>
     </>
