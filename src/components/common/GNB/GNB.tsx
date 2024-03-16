e30bd8908a1d334f9c1d630456260984bd41a9aa
박운성 <cheanju0@gmail.com>
2024-03-16 20:44:25 +0900

    style: 공백 타입 변경

--- src/components/common/GNB/GNB.tsx
+++ src/components/common/GNB/GNB.tsx
@@ -15,87 +15,87 @@ import styles from './GNB.module.scss';
 const cn = classNames.bind(styles);
 
 export default function GNB() {
-	const router = useRouter();
-	const [Auth, setAuth] = useState(false);
-	const { isToggle: isDropdownOpen, handleToggleClick: setIsDropdownOpen } = useToggleButton();
+  const router = useRouter();
+  const [Auth, setAuth] = useState(false);
+  const { isToggle: isDropdownOpen, handleToggleClick: setIsDropdownOpen } = useToggleButton();
 
-	const getMyInfo = async () => {
-		const { data } = await instance.get<myInfoProps>('/users/me');
-		return data;
-	};
+  const getMyInfo = async () => {
+    const { data } = await instance.get<myInfoProps>('/users/me');
+    return data;
+  };
 
-	const { data: MyInfoData, isPending } = useQuery({
-		queryKey: ['myInfo'],
-		queryFn: getMyInfo,
+  const { data: MyInfoData, isPending } = useQuery({
+    queryKey: ['myInfo'],
+    queryFn: getMyInfo,
     retry: false,
-	});
+  });
 
-	const handleLogout = () => {
-		localStorage.removeItem('accessToken');
-		localStorage.removeItem('refreshToken');
-		if (!localStorage.getItem('accessToken')) {
-			setIsDropdownOpen();
-			router.push('/');
-		}
-	};
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    if (!localStorage.getItem('accessToken')) {
+      setIsDropdownOpen();
+      router.push('/');
+    }
+  };
 
   const handleMyPageClick = () => {
     setIsDropdownOpen();
     router.push('/mypage');
+  };
+
+  const MyMenuList = [
+    {
+      text: '내 정보',
+      handleClick: handleMyPageClick,
+    },
+    {
+      text: '로그아웃',
+      handleClick: handleLogout,
+    },
+  ];
+
+  useEffect(() => {
+    if (localStorage.getItem('accessToken')) {
+      setAuth(true);
+      return;
+    }
+    setAuth(false);
+  }, []);
+
+  if (isPending) {
+    return null;
   }
 
-	const MyMenuList = [
-		{
-			text: '내 정보',
-			handleClick: handleMyPageClick,
-		},
-		{
-			text: '로그아웃',
-			handleClick: handleLogout,
-		},
-	];
-
-	useEffect(() => {
-		if (localStorage.getItem('accessToken')) {
-			setAuth(true);
-			return;
-		}
-		setAuth(false);
-	}, []);
-
-	if (isPending) {
-		return null;
-	}
-
-	return (
-		<div className={cn('container')}>
-			<button>
-				<Image src={IMAGE.logo.nav.src} alt={IMAGE.logo.nav.alt} height={28} width={166} />
-			</button>
-			<div>
-				{!Auth ? (
-					<div className={cn('not', 'user')}>
-						<Link href='/signin'>로그인</Link>
-						<Link href='/signup'>회원가입</Link>
-					</div>
-				) : (
-					<div className={cn('user')}>
-						<button className={cn('gnb-button')}>
-							<Image src={ICON.notification.default.src} alt={ICON.notification.default.alt} />
-						</button>
-						<div className={cn('dropdown-menu-box')}>
-							<div className={cn('stroke')} />
-							<div className={cn('user-profile-box')}>
-								<Avatar profileImageUrl={MyInfoData?.profileImageUrl} type='gnb' />
-								<button className={cn('gnb-button')} onClick={setIsDropdownOpen}>
-									{MyInfoData?.nickname}
-								</button>
-								{isDropdownOpen && <DropdownMenu type='gnb' dropdownMenuList={MyMenuList} />}
-							</div>
-						</div>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className={cn('container')}>
+      <button>
+        <Image src={IMAGE.logo.nav.src} alt={IMAGE.logo.nav.alt} height={28} width={166} />
+      </button>
+      <div>
+        {!Auth ? (
+          <div className={cn('not', 'user')}>
+            <Link href='/signin'>로그인</Link>
+            <Link href='/signup'>회원가입</Link>
+          </div>
+        ) : (
+          <div className={cn('user')}>
+            <button className={cn('gnb-button')}>
+              <Image src={ICON.notification.default.src} alt={ICON.notification.default.alt} />
+            </button>
+            <div className={cn('dropdown-menu-box')}>
+              <div className={cn('stroke')} />
+              <div className={cn('user-profile-box')}>
+                <Avatar profileImageUrl={MyInfoData?.profileImageUrl} type='gnb' />
+                <button className={cn('gnb-button')} onClick={setIsDropdownOpen}>
+                  {MyInfoData?.nickname}
+                </button>
+                {isDropdownOpen && <DropdownMenu type='gnb' dropdownMenuList={MyMenuList} />}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/types/myInfo.ts
+++ src/types/myInfo.ts
@@ -1,8 +1,8 @@
 export interface myInfoProps {
-	id: number;
-	email: string;
-	nickname: string;
-	profileImageUrl: null | string;
-	createdAt: string;
-	updatedAt: string;
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: null | string;
+  createdAt: string;
+  updatedAt: string;
 }
