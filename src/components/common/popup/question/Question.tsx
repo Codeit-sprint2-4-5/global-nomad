e74112a7ecb77e05f5884e7420463cb997cd9b9d
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-03-16 19:31:12 +0900

    fix: 여러 페이지에서 쓸 수 있게 수정

--- src/components/common/popup/question/Question.tsx
+++ src/components/common/popup/question/Question.tsx
@@ -1,6 +1,4 @@
 import { RefObject } from 'react';
-import { instance } from '@/apis/axios';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import BaseButton from '@/components/common/button/BaseButton';
 import classNames from 'classnames/bind';
 import styles from './Question.module.scss';
@@ -8,28 +6,15 @@ import styles from './Question.module.scss';
 const cn = classNames.bind(styles);
 
 interface Props {
+  text: string;
   dialogRef: RefObject<HTMLDialogElement>;
-  reservationId: number;
+  buttonText: string;
+  onClick: () => void;
 }
 
-export default function Question({ dialogRef, reservationId }: Props) {
-  async function patchReservation() {
-    await instance.patch(`/my-reservations/${reservationId}`, {
-      status: "canceled",
-    });
-  }
-
-  const queryClient = useQueryClient();
-
-  const { mutate } = useMutation({
-    mutationFn: patchReservation,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["/my-reservations"] });
-    },
-  });
-
-  const handleCancelClick = () => {
-    mutate();
+export default function Question({ dialogRef, text, buttonText, onClick }: Props) {
+  const handleButtonClick = () => {
+    onClick();
     handleCloseClick();
   };
 
@@ -41,10 +26,10 @@ export default function Question({ dialogRef, reservationId }: Props) {
   return (
     <>
       <dialog className={cn('container')} ref={dialogRef}>
-        <p className={cn('text')}>예약을 취소하시겠어요?</p>
+        <p className={cn('text')}>{text}</p>
         <div className={cn('button-group')}>
           <BaseButton onClick={handleCloseClick} size='sm' variant='outline' text='아니오' />
-          <BaseButton onClick={handleCancelClick} size='sm' text='취소하기' />
+          <BaseButton onClick={handleButtonClick} size='sm' text={buttonText} />
         </div>
       </dialog>
     </>
