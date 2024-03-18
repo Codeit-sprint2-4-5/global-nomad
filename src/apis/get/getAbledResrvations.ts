78e135632d6c6ba17cf08bbeb22062e603f3437f
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-03-19 01:22:07 +0900

    fix : 예약 정보 모달 api 커밋에 추가

--- src/apis/get/getAbledResrvations.ts
+++ src/apis/get/getAbledResrvations.ts
@@ -8,17 +8,23 @@ export const getAbledResrvationList = async (activityId = 152, year: number, mon
 export const getMyActivityReservationThisMonth = async (activityId: number, year: number, month: string) => {
   try {
     const res = await instance.get(`/my-activities/${activityId}/reservation-dashboard/`, { params: { year, month } });
-    console.log('월별 예약 리스트', res);
-    return res;
+
+    return res.data;
   } catch (e) {
     console.log(e);
   }
 };
 
+export const getReservedScheduleDate = async (activityId: number, date: string) => {
+  const res = await instance.get(`/my-activities/${activityId}/reserved-schedule`, { params: { date } });
+  const data = res.data;
+  return data;
+};
+
 export const getMyActivitiesReservation = async (
   activityId: number,
   scheduleId: number,
-  status = 'pending',
+  status: 'declined' | 'confirmed' | 'pending',
   size = 10
 ) => {
   try {
@@ -32,8 +38,8 @@ export const getMyActivitiesReservation = async (
   }
 };
 
-export const getMyReserVations = async () => {
-  const res = await instance.get(`/my-reservations?size=10`);
+export const getMyReserVations = async (size: number) => {
+  const res = await instance.get(`/my-reservations`, { params: { size } });
 
   return res.data;
 };
--- src/components/common/Modals/ModalContents/ReservationInfo/ReservationCard.tsx
+++ src/components/common/Modals/ModalContents/ReservationInfo/ReservationCard.tsx
@@ -1,15 +1,41 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import BaseButton from '@/components/common/button/BaseButton';
+import { patchReservationStatus } from '@/apis/patch/patchReservationStatus';
 import ReservationTag from '@/components/common/ReservationTag';
 import classNames from 'classnames/bind';
-import styles from './Reservaiton.module.scss';
-import BaseButton from '@/components/common/button/BaseButton';
+import styles from './Reservation.module.scss';
 const cn = classNames.bind(styles);
 
 interface ReservationStatusProps {
-  selected: 'pending' | 'declined' | 'confirmed';
+  selectedStatus: 'pending' | 'declined' | 'confirmed';
+  reservationId: number;
+  activityId: number;
 }
-function ReservationStatus({ selected }: ReservationStatusProps) {
+interface Props extends ReservationStatusProps {
+  nickname: string;
+  headCount: number;
+  status: ReservationStatusProps['selectedStatus'];
+}
+function ReservationStatus({ selectedStatus, reservationId, activityId }: ReservationStatusProps) {
+  const queryClient = useQueryClient();
+
   //버튼 클릭시 status 변경하는 프롭 받아오기
-  switch (selected) {
+
+  console.log(reservationId);
+
+  const patchStatusMutation = useMutation({
+    mutationFn: (data: { status: ReservationStatusProps['selectedStatus'] }) =>
+      patchReservationStatus(activityId, reservationId, data),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['my-activities', 'reservation', 'time'] }),
+  });
+  const handleClickConfirmed = () => {
+    patchStatusMutation.mutate({ status: 'confirmed' });
+  };
+
+  const handleClickDeclined = () => {
+    patchStatusMutation.mutate({ status: 'declined' });
+  };
+  switch (selectedStatus) {
     case 'pending':
       return (
         <>
@@ -25,23 +51,36 @@ function ReservationStatus({ selected }: ReservationStatusProps) {
       return <ReservationTag status='confirmed' />;
       break;
     default:
-      return <p>ddddd</p>;
+      return;
       break;
   }
 }
 
-export default function ReservationCard({ selected }: ReservationStatusProps) {
-  return (
-    <li className={cn('reservation-card-item')}>
-      <p className={cn('reservation-card-item-info')}>
-        닉네임<span>정만철</span>
-      </p>
-      <p className={cn('reservation-card-item-info')}>
-        인원<span>12명</span>
-      </p>
-      <div className={cn('reservation-card-item-status')}>
-        <ReservationStatus selected={selected} />
-      </div>
-    </li>
-  );
+export default function ReservationCard({
+  selectedStatus,
+  nickname,
+  headCount,
+  status,
+  reservationId,
+  activityId,
+}: Props) {
+  if (status === selectedStatus) {
+    return (
+      <li className={cn('reservation-card-item')}>
+        <p className={cn('reservation-card-item-info')}>
+          닉네임<span>{nickname}</span>
+        </p>
+        <p className={cn('reservation-card-item-info')}>
+          인원<span>{headCount}명</span>
+        </p>
+        <div className={cn('reservation-card-item-status')}>
+          <ReservationStatus activityId={activityId} reservationId={reservationId} selectedStatus={selectedStatus} />
+        </div>
+      </li>
+    );
+  } else {
+    return <li className={cn('reservation-card-item-none')}>해당하는 예약 내역이 없습니다</li>;
+  }
+
+  //여기서 처리해주면안됨 제대로 처리해야=
 }
