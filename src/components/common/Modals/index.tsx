c719a1fe13382b20170da758e5669d4781512a2d
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-03-19 01:13:39 +0900

    feat : 예약 정보 모달 구현

--- src/components/common/Modals/index.tsx
+++ src/components/common/Modals/index.tsx
@@ -8,7 +8,6 @@ import ReservationInfo from './ModalContents/ReservationInfo/ReservationInfo';
 import Review from './ModalContents/Review/Review';
 import Notifications from './ModalContents/Notifications/Notifications';
 import DateForm from './ModalContents/dateForm/DateForm';
-
 import { ICON } from '@/constants';
 import styles from './Modal.module.scss';
 import classNames from 'classnames/bind';
@@ -29,43 +28,52 @@ interface ModalProps {
   modalType: keyof typeof MODAL_TYPE;
   setShowModal: Dispatch<SetStateAction<string>>;
   control?: Control<any>;
-  data?: any;
   id?: number;
+  abledReservationListData?: AbledReservationListData[];
   setValue?: UseFormSetValue<any>;
   onDownDisabled?: boolean;
 }
 
 export default function Modal({ modalType, setShowModal, ...props }: ModalProps) {
-  const handleClickCloseModal = () => {
+  const handelClickCloseModal = () => {
     setShowModal('');
   };
   const ModalContents = {
     [MODAL_TYPE.review]: {
       component: Review,
-      prop: { id: props.id as number, onClickCloseModal: handleClickCloseModal },
+      prop: { id: props.id as number, onClickCloseModal: handelClickCloseModal },
     },
-    [MODAL_TYPE.reservationInfo]: { component: ReservationInfo, props: {} },
+    [MODAL_TYPE.reservationInfo]: { component: ReservationInfo, prop: {} },
     [MODAL_TYPE.dateForm]: {
       component: DateForm,
-      prop: { onClickCloseModal: handleClickCloseModal, control: props.control },
+      prop: {
+        onClickCloseModal: handelClickCloseModal,
+        control: props.control,
+        abledReservationListData: props.abledReservationListData,
+      },
     },
     [MODAL_TYPE.notifications]: { component: Notifications, prop: {} },
     [MODAL_TYPE.countMemberInput]: {
       component: CountMemberInput,
-      prop: { control: props.control, setValue: props.setValue, onDownDisabled: props.onDownDisabled },
+      prop: {
+        setValue: props.setValue,
+        control: props.control,
+        onDownDisabled: props.onDownDisabled,
+      },
     },
   };
+
   const { component: ContestComponent, prop } = ModalContents[modalType];
 
   return createPortal(
     <>
       <section className={cn('modal-content', { notifications: modalType === 'notifications' })}>
-        <Image src={x.default.src} alt={x.default.alt} width={40} height={40} onClick={handleClickCloseModal} />
-        <ContestComponent onClickCloseModal={handleClickCloseModal} {...prop} />
+        <Image src={x.default.src} alt={x.default.alt} width={40} height={40} onClick={handelClickCloseModal} />
+        <ContestComponent onClickCloseModal={handelClickCloseModal} {...prop} />
       </section>
       <div
         className={cn('modal-background', { 'no-background': modalType !== 'review' })}
-        onClick={handleClickCloseModal}
+        onClick={handelClickCloseModal}
       ></div>
     </>,
     document.body
