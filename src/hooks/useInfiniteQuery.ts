45273baf46291089503b6a34f7eb9b63896db9a9
JaeHwan <131963909+BBoBBoM@users.noreply.github.com>
2024-03-18 20:33:10 +0900

    feat: Mypage-myActivities 페이지 구현

--- src/hooks/useInfiniteQuery.ts
+++ src/hooks/useInfiniteQuery.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, QueryFunction } from "@tanstack/react-query";
+import { useInfiniteQuery, QueryFunction } from '@tanstack/react-query';
 
 interface useCustomInfiniteQueryProps {
   queryKey: string[];
