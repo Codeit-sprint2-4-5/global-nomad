9bae126ffff3d02860d374115b2a2f1e6fed6939
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-03-19 01:24:40 +0900

    chore : 날짜 모달 스타일  구현

--- src/components/common/Modals/ModalContents/dateForm/DateForm.tsx
+++ src/components/common/Modals/ModalContents/dateForm/DateForm.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import { Controller, Control, UseFormGetValues } from 'react-hook-form';
-import Calender from '@/components/common/Modals/ModalContents/dateForm/calendar/Calender';
+import { Controller, Control, UseFormGetValues, useWatch, UseFormSetValue } from 'react-hook-form';
 import BaseButton from '@/components/common/button/BaseButton';
 
 import styles from './DateForm.module.scss';
 import classNames from 'classnames/bind';
 import { AbledReservationListData } from '@/components/common/floatingBox/FloatingBox';
+import SmallCalender from './smallCalendar/SmallCalender';
 
 const cn = classNames.bind(styles);
 
-interface AbledReservationListDataItem {
+export interface AbledReservationListDataType {
   date: string;
   times: {
     id: number;
@@ -24,49 +24,44 @@ interface DateFormProps {
   onClickCloseModal?: () => void;
   className?: string;
   getValues?: UseFormGetValues<any>;
+  setValue?: UseFormSetValue<any>;
 }
 
-export default function DateForm({ control, abledReservationListData, onClickCloseModal, className }: DateFormProps) {
+export default function DateForm({
+  control,
+  abledReservationListData,
+  onClickCloseModal,
+  className,
+  setValue,
+}: DateFormProps) {
   const [disabled, setDisabled] = useState(false);
-  const abledDate = abledReservationListData;
+  const abledDate = abledReservationListData?.map((date: any) => date.date);
 
   console.log('날짜 데이터', abledDate);
+
   return (
     <>
       <h2 className={cn('title', className)}>날짜</h2>
       <div className={cn('date-form', className)}>
-        <Calender control={control} />
+        <SmallCalender name='date' abledDate={abledDate} control={control} setValue={setValue} />
         <div className={cn('abled-time')}>
-          <label htmlFor="abled-time" className={cn('abled-time-label')}>
+          <label htmlFor='abled-time' className={cn('abled-time-label')}>
             예약 가능한 시간
           </label>
           <ul className={cn('abled-time-input-list')}>
-            {abledDate ? (
-              abledDate[0].times.map((time) => (
+            {/* {abledDate?..map(
+              (time) => (
                 // date.times.map((time) =>
                 <li key={time.id}>
-                  <Controller
-                    control={control}
-                    name="abled-time"
-                    render={({ ...field }) => (
-                      <input
-                        id="abled-time"
-                        type="button"
-                        {...field}
-                        className={cn('abled-time-input-item')}
-                        value={`${time.startTime}~${time.endTime}`}
-                        disabled={disabled}
-                      />
-                    )}
-                  />
+                  <BaseButton text={`${time.startTime}~${time.endTime}`} size='sm' variant={'outline'} />
                 </li>
-              ))
-            ) : (
-              <p key="dd">해당하는 날짜에 예약 가능한 시간이 없습니다</p>
-            )}
+              )
+
+              // <p key='dd'>해당하는 날짜에 예약 가능한 시간이 없습니다</p>
+            )} */}
           </ul>
         </div>
-        {onClickCloseModal && <BaseButton onClick={onClickCloseModal} type="button" text="작성하기" size="lg" />}
+        {onClickCloseModal && <BaseButton onClick={onClickCloseModal} type='button' text='작성하기' size='lg' />}
       </div>
     </>
   );
