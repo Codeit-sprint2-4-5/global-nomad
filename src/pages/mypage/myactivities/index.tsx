f4e8f5efb26d6b0ab3b9567e0094b929be0cd71e
JaeHwan <131963909+BBoBBoM@users.noreply.github.com>
2024-03-19 09:44:36 +0900

    refactor: css부분 수정

---
+++ src/pages/mypage/myactivities/index.tsx
@@ -0,0 +1,10 @@
+import MyActivities from '@/components/domain/mypage/myActivities/MyActivities';
+import MyReservations from '@/components/domain/mypage/myReservations/MyReservations';
+
+export default function Page() {
+  return (
+    <>
+      <MyActivities />
+    </>
+  );
+}
---
+++ src/pages/mypage/myreservations/index.tsx
@@ -0,0 +1,9 @@
+import MyReservations from '@/components/domain/mypage/myReservations/MyReservations';
+
+export default function Page() {
+  return (
+    <>
+      <MyReservations />
+    </>
+  );
+}
