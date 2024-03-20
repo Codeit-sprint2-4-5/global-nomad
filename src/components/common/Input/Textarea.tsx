4e93820d1d3cf408d404e9982d5b56ec719c97c5
seongseonko <kymgok1001@naver.com>
2024-03-20 22:23:04 +0900

    feat: TextArea 컴포넌트 forwardRef 반영

--- src/components/common/Input/Textarea.tsx
+++ src/components/common/Input/Textarea.tsx
@@ -1,22 +1,24 @@
 import classNames from 'classnames/bind';
 import style from '@/components/common/Input/inputField.module.scss';
-import { DetailedHTMLProps, TextareaHTMLAttributes } from 'react';
+import { DetailedHTMLProps, TextareaHTMLAttributes, forwardRef } from 'react';
 
 const cn = classNames.bind(style);
 
 interface TextareaProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
-  name: string;
   isError?: boolean;
   errorMessage?: string;
 }
 
-export default function Textarea({ name, isError, errorMessage, ...props }: TextareaProps) {
+export default forwardRef<HTMLTextAreaElement, TextareaProps>(function Textarea(
+  { isError, errorMessage, ...props },
+  ref
+) {
   return (
     <>
       <div className={cn('textarea-field')}>
-        <textarea {...props} name={name} className={cn('textarea', { error: isError })} />
+        <textarea {...props} ref={ref} className={cn('textarea', { error: isError })} />
       </div>
       {isError && <p className={cn('error')}>{errorMessage}</p>}
     </>
   );
-}
+});
