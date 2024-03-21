2435ac3fe176c6912b2e09ed69790f43c2d4c3db
seongseonko <kymgok1001@naver.com>
2024-03-21 16:42:27 +0900

    feat: mypage, mypage[id] 페이지 resize debounce적용

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -3,13 +3,14 @@ import Layout from '@/components/common/layout/Layout';
 import { useEffect } from 'react';
 import Router from 'next/router';
 import useRouteStore from '@/stores/useRoute';
+import debounce from '@/function/debounce';
 
 export default function Mypage() {
-  const handleResize = () => {
+  const handleResize = debounce(() => {
     if (window.innerWidth > 767) {
       Router.push('/mypage/myinfo', undefined, { shallow: true });
     }
-  };
+  }, 20);
 
   useEffect(() => {
     handleResize();
