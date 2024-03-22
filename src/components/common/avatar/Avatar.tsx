a0a27e453c98d9f0691d71b2c305ac109bf359cf
운성 <86371724+Useong0@users.noreply.github.com>
2024-03-22 14:41:57 +0900

    Feat: GNB 컴포넌트 구현 (#104)
    * feat: Avatar 컴포넌트 구현
    
    * feat: Avatar 컴포넌트 props 기본값 추가
    
    * feat: myInfo 타입 추가
    
    * feat: GNB 컴포넌트 구현
    
    * style: 공백 타입 변경
    
    * feat: 로고 Link 추가
    
    * feat: 레이아웃에 GNB 컴포넌트 추가

---
+++ src/components/common/avatar/Avatar.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import classNames from 'classnames/bind';
+import styles from './Avatar.module.scss';
+import { IMAGE } from '@/constants/importImages';
+
+const cn = classNames.bind(styles);
+
+interface AvatarProps {
+  profileImageUrl?: string | null;
+  type: string;
+}
+
+export default function Avatar({ profileImageUrl = null, type }: AvatarProps) {
+  const imageUrl = profileImageUrl || IMAGE.avatar.default.src;
+  return (
+    <div className={cn('container', type)}>
+      <Image className={cn(type)} src={imageUrl} alt='프로필 이미지' height={45} width={45} />
+    </div>
+  );
+}
