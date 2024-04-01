2a4624d5b4118316dc2c1b66df8cb937249e410f
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-04-01 16:36:19 +0900

    Feat/calendar/mijin0928 (#176)
    * feat: 달력 api 연결
    
    * feat: 달력 컴포넌트 구현
    
    * fix: 드롭다운 undefined 오류 수정
    
    * refactor: Days 컴포넌트 분리
    
    * feat: 등록된 체험이 없는 경우 추가
    
    * feat: 달력 api 연결
    
    * feat: 달력 컴포넌트 구현
    
    * fix: 드롭다운 undefined 오류 수정
    
    * refactor: Days 컴포넌트 분리
    
    * feat: 예약 정보 모달 연결
    
    * feat: 예약모달에 무한스크롤 적용
    
    * feat: 예약현황 페이지 적용

--- src/pages/myreservedstatus/index.tsx
+++ src/pages/myreservedstatus/index.tsx
@@ -1,18 +1,9 @@
-import Layout from '@/components/common/layout/Layout';
 import Calendar from '@/components/domain/myreservedstatus/Calendar';
-import SideNavMenu from '@/components/common/sideNavMenu/SideNavMenu';
 import classNames from 'classnames/bind';
 import styles from '@/components/domain/myreservedstatus/Calendar.module.scss';
 
 const cn = classNames.bind(styles);
 
 export default function ReservationStatus() {
-  return (
-    <>
-      <div className={cn('wrap')}>
-        <SideNavMenu />
-        <Calendar />
-      </div>
-    </>
-  );
+  return <Calendar />;
 }
