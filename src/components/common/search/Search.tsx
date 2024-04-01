0ddb685522cbdafd1cfd1dcf3ad555343473b07c
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-04-01 14:06:01 +0900

    feat: 검색 컴포넌트 추가 기능 구현 (#160)
    * feat: 버튼 컴포넌트 적용
    
    * fix: 세터함수 직접 내려주는 부분 수정
    
    * feat: 디바운싱 구현
    
    * fix: 디바운싱 오류 수정
    
    * feat: 모든 체험 타이틀 불러와 검색바에 보여주기
    
    * feat: 검색바 포커스 시 애니메이션 효과

--- src/components/common/search/Search.tsx
+++ src/components/common/search/Search.tsx
@@ -1,7 +1,9 @@
-import { ChangeEvent, FormEvent, useState, useRef } from 'react';
+import { ChangeEvent, FormEvent, useState, useRef, useEffect } from 'react';
 import BaseButton from '@/components/common/button/BaseButton';
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
+import { instance } from '@/apis/axios';
+import { useQuery } from '@tanstack/react-query';
 
 const cn = classNames.bind(styles);
 interface Props {
@@ -12,13 +14,37 @@ interface Props {
 
 export default function Search({ keyword, onSubmit, onChange }: Props) {
   const [isKeyword, setIsKeyword] = useState(false);
+  const [title, setTitle] = useState('');
+  const [isFocus, setIsFocus] = useState(false);
   const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  async function getActivity() {
+    const res = await instance.get('/activities', {
+      params: {
+        method: 'offset',
+      },
+    });
+    return res.data;
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['/activities'],
+    queryFn: getActivity,
+  });
+
+  const handleSearchFocus = () => {
+    setIsFocus(true);
+    setIsKeyword(true);
+  };
 
   const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
     onChange(e);
 
     if (e.target.value === '') {
       setIsKeyword(false);
+    } else {
+      setIsKeyword(true);
+      setIsFocus(false);
     }
 
     if (timerRef.current) {
@@ -35,25 +61,36 @@ export default function Search({ keyword, onSubmit, onChange }: Props) {
     if (!keyword) return;
 
     onSubmit();
-    setIsKeyword(true);
   };
+
+  let count = 1;
+  useEffect(() => {
+    setTitle(data?.activities[0].title);
+
+    const title = setInterval(() => {
+      if (count < data?.activities.length) {
+        setTitle(data?.activities[count++].title);
+      }
+    }, 3000);
+
+    return () => clearInterval(title);
+  }, []);
+
+  if (isLoading) return;
 
   return (
     <div className={cn('container')}>
       <p className={cn('text')}>무엇을 체험하고 싶으신가요?</p>
       <div className={cn('wrap')}>
-        <form onSubmit={handleSubmit} className={cn('form', { acitve: isKeyword })}>
-          <input
-            className={cn('search-bar')}
-            type='search'
-            placeholder='내가 원하는 체험은'
-            onChange={handleValueChange}
-            value={keyword}
-          />
+        <form onSubmit={handleSubmit} className={cn('form', { acitve: isKeyword })} onFocus={handleSearchFocus}>
+          <input className={cn('search-bar')} type='search' onChange={handleValueChange} value={keyword} />
           <div className={cn('button')}>
             <BaseButton type='submit' size='md' text='검색하기' />
           </div>
         </form>
+        <div className={cn('title')}>
+          <ul>{!keyword && !isFocus && <li>{title}</li>}</ul>
+        </div>
       </div>
     </div>
   );
