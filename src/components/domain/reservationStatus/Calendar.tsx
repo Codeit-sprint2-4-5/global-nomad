5f741b220ea13717161d213d2d0d6a3afbcdcb22
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-04-01 15:55:14 +0900

    feat: 예약현황 페이지 적용

--- src/components/domain/reservationStatus/Calendar.tsx
+++ src/components/domain/reservationStatus/Calendar.tsx
@@ -4,7 +4,6 @@ import { instance } from '@/apis/axios';
 import Dropdown from '@/components/common/dropdown/Dropdown';
 import NoDataMessage from '@/components/common/noDataMessgae/NoDataMessage';
 import Modal from '@/components/common/Modals';
-import ReservationInfo from '@/components/common/Modals/ModalContents/reservationInfo/ReservationInfo';
 import Days from './Days';
 import classNames from 'classnames/bind';
 import styles from './Calendar.module.scss';
@@ -157,7 +156,6 @@ export default function Calendar() {
             activityId={activityId}
           />
         )}
-        {/* <ReservationInfo /> */}
       </div>
     </div>
   );
