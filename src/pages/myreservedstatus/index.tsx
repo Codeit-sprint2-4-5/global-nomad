b71284d1c23111991aebe258f1619c0074f352fd
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-04-02 14:49:01 +0900

    fix: 파일 수정

--- src/pages/myreservedstatus/index.tsx
+++
@@ -1,9 +0,0 @@
-import Calendar from '@/components/domain/myreservedstatus/Calendar';
-import classNames from 'classnames/bind';
-import styles from '@/components/domain/myreservedstatus/Calendar.module.scss';
-
-const cn = classNames.bind(styles);
-
-export default function ReservationStatus() {
-  return <Calendar />;
-}
